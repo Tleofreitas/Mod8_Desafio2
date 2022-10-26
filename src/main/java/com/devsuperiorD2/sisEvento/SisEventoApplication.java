78d7bbcd34e57d67d77e26b79f050259c8fb250f
Thiago Freitas <thiagoleonardo1995@live.com>
2022-10-25 23:33:44 -0300

    Completo

---
+++ src/main/java/com/devsuperiorD2/sisEvento/SisEventoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperiorD2.sisEvento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisEventoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/devsuperiorD2/sisEvento/entities/Categoria.java
@@ -0,0 +1,50 @@
+package com.devsuperiorD2.sisEvento.entities;
+
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Mapeamento do JPA
+@Entity
+@Table(name = "tb_categoria") // Nome da tabela no banco de dados
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Id auto incrementável
+    private Integer id;
+    @Column(columnDefinition = "TEXT") // Para poder receber texto longo
+    private String descricao;
+
+    // Relacionamento UM para MUITOS com Atividade
+    @OneToMany(mappedBy = "categoria")
+    private List<Atividade> atividade = new ArrayList<>();
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Atividade> getAtividade() {
+        return atividade;
+    }
+}
